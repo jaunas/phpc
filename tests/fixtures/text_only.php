d68b2ebe3d1bf3e883066bf814a00b31b88b518f
jaunas <93.tomek@gmail.com>
2023-11-26 19:49:29 +0100

    Add GitHub Actions

--- tests/fixtures/text_only.php
+++ tests/fixtures/text_only.php
@@ -1,5 +1,17 @@
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis tincidunt eros. Donec posuere nibh a molestie malesuada. Nulla fringilla efficitur neque ac volutpat. Praesent faucibus eleifend accumsan. Donec aliquam nisl ut pulvinar consequat. Morbi non elit a augue cursus egestas. Vivamus turpis nisi, condimentum vel massa vitae, fringilla aliquam tortor. Nullam ac augue iaculis, euismod tortor at, volutpat lacus. Aliquam placerat ultricies purus, vitae tempus justo convallis et.
-In ac elementum nunc. Phasellus et dapibus felis. Vivamus molestie ipsum at tortor iaculis tempus. Aliquam nulla magna, consectetur quis justo ac, consectetur gravida nisi. In mattis eget leo in ullamcorper. Duis porta pellentesque leo, eu suscipit nibh cursus eget. Fusce sed condimentum magna.
-Vestibulum vulputate turpis eu scelerisque lacinia. Aliquam volutpat dui sit amet felis viverra, quis lacinia mauris venenatis. In ut ullamcorper urna. Praesent vitae est purus. Fusce quis ex venenatis, tristique magna vel, ornare risus. Praesent sed felis eleifend, luctus lorem ac, fringilla nunc. Integer ullamcorper, ex eget euismod iaculis, est nisi feugiat arcu, quis gravida dui arcu eu velit. Nullam eu tortor vel ligula suscipit tristique.
+<?php
 
-Pellentesque odio lorem, semper in nulla at, tempus interdum turpis. Cras ultricies interdum aliquet. Donec elementum elementum arcu, in tempus elit.
+?>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis tincidunt eros. Donec posuere nibh a molestie
+malesuada. Nulla fringilla efficitur neque ac volutpat. Praesent faucibus eleifend accumsan. Donec aliquam nisl ut
+pulvinar consequat. Morbi non elit a augue cursus egestas. Vivamus turpis nisi, condimentum vel massa vitae, fringilla
+aliquam tortor. Nullam ac augue iaculis, euismod tortor at, volutpat lacus. Aliquam placerat ultricies purus, vitae
+tempus justo convallis et.
+In ac elementum nunc. Phasellus et dapibus felis. Vivamus molestie ipsum at tortor iaculis tempus. Aliquam nulla magna,
+consectetur quis justo ac, consectetur gravida nisi. In mattis eget leo in ullamcorper. Duis porta pellentesque leo, eu
+suscipit nibh cursus eget. Fusce sed condimentum magna.
+Vestibulum vulputate turpis eu scelerisque lacinia. Aliquam volutpat dui sit amet felis viverra, quis lacinia mauris
+venenatis. In ut ullamcorper urna. Praesent vitae est purus. Fusce quis ex venenatis, tristique magna vel, ornare risus.
+Praesent sed felis eleifend, luctus lorem ac, fringilla nunc. Integer ullamcorper, ex eget euismod iaculis, est nisi
+feugiat arcu, quis gravida dui arcu eu velit. Nullam eu tortor vel ligula suscipit tristique.
+
+Pellentesque odio lorem, semper in nulla at, tempus interdum turpis. Cras ultricies interdum aliquet. Donec elementum
+elementum arcu, in tempus elit.
