324c1ac99dc1ba2f4b9ccf8c50d8b4edd2df6680
jaunas <93.tomek@gmail.com>
2024-02-23 16:42:33 +0100

    Add Number variant to Value

--- rust-php/src/lib.rs
+++ rust-php/src/lib.rs
@@ -10,11 +10,10 @@ impl PhpNumber {
     }
 
     fn trim_leading_zeroes(&self) -> String {
-        let with_precision = format!("{:.13}", self.value);
-        return with_precision
+        format!("{:.13}", self.value)
             .trim_end_matches('0')
             .trim_end_matches('.')
-            .to_owned();
+            .to_owned()
     }
 }
 
@@ -28,11 +27,19 @@ impl fmt::Display for PhpNumber {
 pub enum Value {
     Null,
     String(String),
+    Number(f64),
 }
 
 impl Value {
     pub fn concat(self, other: Self) -> Self {
         Value::String(self.to_string() + &other.to_string())
+    }
+
+    fn trim(number: f64) -> String {
+        format!("{:.13}", number)
+        .trim_end_matches('0')
+        .trim_end_matches('.')
+        .to_string()
     }
 }
 
@@ -41,22 +48,14 @@ impl fmt::Display for Value {
         match self {
             Value::Null => write!(f, ""),
             Value::String(string) => write!(f, "{}", string),
+            Value::Number(number) => write!(f, "{}", Value::trim(*number)),
         }
     }
-}
-
-pub fn concat(left: Value, right: Value) -> Value {
-    Value::String(left.to_string() + &right.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn create_null_value() {
-        let _null = Value::Null;
-    }
 
     #[test]
     fn compare_nulls() {
@@ -69,11 +68,6 @@ mod tests {
     fn null_to_string() {
         let null = Value::Null;
         assert_eq!("", null.to_string());
-    }
-
-    #[test]
-    fn create_string_value() {
-        let _string = Value::String("example string".to_string());
     }
 
     #[test]
@@ -110,5 +104,17 @@ mod tests {
 
         let concat = left_string.concat(right_string);
         assert!(Value::String("leftright".to_string()) == concat);
+    }
+
+    #[test]
+    fn number_int_to_string() {
+        let number = Value::Number(5_f64);
+        assert_eq!("5", number.to_string());
+    }
+
+    #[test]
+    fn number_float_to_string() {
+        let number = Value::Number(5_f64/3_f64);
+        assert_eq!("1.6666666666667", number.to_string());
     }
 }
